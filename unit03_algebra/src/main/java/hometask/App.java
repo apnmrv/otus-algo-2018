79fa7376eb2bf8e9abeff7c2c0b18abb5360fd0d
apnmrv <apnmrv@gmail.com>
2019-01-09 21:18:59 +0300

    hometask template added

---
+++ unit03_algebra/src/main/java/hometask/App.java
@@ -0,0 +1,11 @@
+package hometask;
+/*
+* В дз надо реализовать алгоритм умножения матриц и его испольовать для алгоритмма быстрого возведения в степень?
+* */
+
+public class App {
+    public static void main(String...args)
+    {
+
+    }
+}
