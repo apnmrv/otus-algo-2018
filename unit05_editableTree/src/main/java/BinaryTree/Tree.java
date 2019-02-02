4ad37b0ba64d7daacc876a24835da35e9fe1cd26
apnmrv <apnmrv@gmail.com>
2019-02-02 18:28:58 +0300

    blueprint

---
+++ unit05_editableTree/src/main/java/BinaryTree/Tree.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+class Tree {
+
+    private int[] _data;
+    private int _lastChildIdx;
+    private int _lastParentIdx;
+    private int[] _parents;
+    private int[] _leafs;
+
+    Tree() { }
+}
